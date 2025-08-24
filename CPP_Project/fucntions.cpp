e755fe1e90c3734dc4a43376fa1fe23bdffb8efc
mahivellanki <mahivellanki557@gmail.com>
2025-08-24 17:00:09 +0000

    fucntions

---
+++ CPP_Project/fucntions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string fname) {
+  cout << fname << " Vellanki\n";
+}
+
+int main() {
+  myFunction("mahesh");
+  myFunction("Kiran");
+  myFunction("Anil");
+  myfunction("anusha");
+  myfucntion("narendra");
+  return 0;
+}
